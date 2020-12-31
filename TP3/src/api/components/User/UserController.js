ba75de1014f6c5abeb9555726932f736173375be
TheFlover <florian.coulange@gmail.com>
2020-12-31 17:27:15 +0100

    tp 3

--- TP3/src/api/components/User/UserController.js
+++ TP3/src/api/components/User/UserController.js
@@ -1,4 +1,3 @@
-let users = require('./UserData');
 const User = require('./models/UserModel');
 
 //READ ALL
@@ -40,26 +39,17 @@ exports.createOne = (req, res) =>
         return res.status(400).json(err);
     });
 }
+
 exports.editOne = (req, res) =>
 {
-    let user = users.find(user => user.id == req.params.id);
-
-    if (!user)
-    {
-        return res.status(404).json({result: `id ${req.params.id} not found`});
-    }
-    for(const key in req.body)
-        {
-            if(user.hasOwnProperty(key))
-            {
-                if(key != 'id')
-                {
-                    user[key] = req.body[key];
-                }
-            }
-            
-        }
-    res.status(200).json(user);
+    User.findByIdAndUpdate(req.params.id, req.body)
+        .exec()
+        .then(result => {
+            return res.status(200).json("L'utilisateur a été modifié.");
+        })
+        .catch(err => {
+            return res.status(400).json(err);
+        })
 }
 
 
